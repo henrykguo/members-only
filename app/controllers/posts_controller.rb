87ab053229a462cbef48dc92100e63ca377a51a3
Henry <83632536+henrykguo@users.noreply.github.com>
2025-09-08 21:01:19 -0400

    add user sign in and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
   def index
-  end
-
-  def show
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "Post created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def edit
-  end
+  private
 
-  def update
-  end
-
-  def destroy
+  def post_params
+    params.expect(post: [ :title, :body ])
   end
 end
